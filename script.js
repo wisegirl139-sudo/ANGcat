f8054d073019225e16204a385e306ec1aa07f0ce
Aanya <wisegirl139@gmail.com>
2026-08-19 03:59:36 +0100

    sdh

--- script.js
+++ script.js
@@ -2,5 +2,35 @@ const cat = document.getElementById("maincat");
 const moods = document.getElementById("moods")
 
 cat.addEventListener("click",function(){
+    cat.src = "images/click.png"
     moods.style.display = "block";
+})
+
+const happy = document.getElementById("happy");
+const sad = document.getElementById("sad");
+const angry = document.getElementById("angry");
+const anxious = document.getElementById("anxious");
+
+happy.addEventListener("click",function(){
+    sad.style.display="none";
+    angry.style.display="none";
+    anxious.style.display="none";
+})
+
+sad.addEventListener("click",function(){
+    happy.style.display="none";
+    angry.style.display="none";
+    anxious.style.display="none";
+})
+
+angry.addEventListener("click",function(){
+    happy.style.display="none";
+    sad.style.display="none";
+    anxious.style.display="none";
+})
+
+angry.addEventListener("click",function(){
+    happy.style.display="none";
+    sad.style.display="none";
+     angry.style.display="none";
 })
