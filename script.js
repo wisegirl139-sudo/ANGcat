e7e57219ad43e04645ab267d941beb0e3fc818e2
Aanya <wisegirl139@gmail.com>
2026-08-19 22:58:39 +0100

    omds hy r they so big

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ happy.addEventListener("click",function(){
     cat.src = "images/happy.png"
     music.innerHTML='<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4sKdJxZR5UfG69CvBmJCwc?utm_source=generator&si=21604b7c917847a3" width="60%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     document.body.className="happybg"
+    startNotes();
 })
 
 sad.addEventListener("click",function(){
@@ -28,6 +29,7 @@ sad.addEventListener("click",function(){
     cat.src = "images/sad.png"
     music.innerHTML='<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/7vWNLnchz6Q7S2fMCWuhAO?utm_source=generator&si=2411de3f61e74454" width="60%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     document.body.className="sadbg"
+    startNotes();
 })
 
 angry.addEventListener("click",function(){
@@ -37,6 +39,7 @@ angry.addEventListener("click",function(){
     cat.src = "images/angry.png"
     music.innerHTML='<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6z3W7qKlkaktbYvqEHYato?utm_source=generator&si=c4cf911ff026483c" width="60%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     document.body.className="angrybg"
+    startNotes();
 })
 
 anxious.addEventListener("click",function(){
@@ -46,6 +49,7 @@ anxious.addEventListener("click",function(){
     cat.src = "images/anxious.png"
     music.innerHTML='<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/2xhRAbLHzmuzq7e0R9NEbn?utm_source=generator&theme=0&si=b73a92474b8a4849" width="60%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     document.body.className="anxiousbg"
+    startNotes();
 })
 
 const notes = document.getElementById("notes");
@@ -64,4 +68,11 @@ function createNote(){
     setTimeout(function(){
         note.remove();
     }, 4000);
+}
+let notesStarted =  false;
+function startNotes(){
+    if (!notesStarted){
+        setInterval(createNote,700);
+        notesStarted = true;
+    }
 }
